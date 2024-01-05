3d547fdc7c1d3586ea6be35f9253caec2c23284c
Julius Esclamado <esclamadojp@gmail.com>
2024-01-05 19:41:32 +0800

    final change

--- src/pages/HoProfiles.vue
+++ src/pages/HoProfiles.vue
@@ -1,31 +1,57 @@
 <template>
   <div class="q-pa-md">
-    <q-table
-      :rows="tablerows"
-      :columns="columns"
-      row-key="id"
-      v-model:selected="selected"
-      dense
-      :separator="separator"
-      :filter="filter"
-      :loading="loading"
-      :pagination="{rowsPerPage:100}"
-      >
-  
-      <template v-slot:loading>
-        <q-inner-loading showing color="primary" />
-      </template>
-      
-    </q-table>
-  </div>
+    <q-form @submit="onSubmit" class="q-gutter-md">
+      <q-radio name="mygroup" v-model="mygroup" val=6 label="Country" />
+      <q-radio name="mygroup" v-model="mygroup" val=7 label="State" />
+      <q-radio name="mygroup" v-model="mygroup" val=8 label="City" />
+      <q-radio name="mygroup" v-model="mygroup" val=10 label="Zip" />
+    </q-form>
 
+    <table >
+      <tbody>
+        <tr v-for="(records, groupings) in groupData" :key="groupings">
+          <td style="vertical-align: top; text-align: left;">{{ groupings }}</td>
+          <td>
+            <table border="1" style="border-style: none;border-width: thin;">
+              <tr>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>eMail</th>
+                  <th>Gender</th>
+                  <th>Country</th>
+                  <th>State</th>
+                  <th>City</th>
+                  <th>Street</th>
+                  <th>Zip</th>
+                  <th>ARR</th>
+                </tr>
+              <tr v-for="record in records" :key="record.Firstname">
+                <td width="15%">{{ record.Firstname }} </td>
+                <td width="15%">{{ record.Lastname }} </td>
+                <td width="15%">{{ record.eMail }}</td>
+                <td width="5%">{{ record.Gender }}</td>
+                <td width="10%">{{ record.Country }}</td>
+                <td width="10%">{{ record.State }}</td>
+                <td width="10%">{{ record.City }}</td>
+                <td width="10%">{{ record.Street }}</td>
+                <td width="5%">{{ record.Zip }}</td>
+                <td width="5%">{{ record.ARR }}</td>
+              </tr>
+              <tr>
+                <td colspan="10" style="text-align: right;">Total ARR: {{ getTotalArr(records) }}</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
-    import { ref } from 'vue'
-    import { exportFile, useQuasar } from 'quasar'
+    import { computed, ref } from 'vue'
+    import { useQuasar } from 'quasar'
     import { api } from 'boot/axios' 
-    import axios from 'axios'
 
     const $q = useQuasar()
     const selected = ref([])
@@ -33,6 +59,7 @@
     const tablerows = ref([])
     const loading = ref(false)
     const filter = ref('')
+    const mygroup = ref(6)
     
     const columns = [
       {
@@ -40,26 +67,25 @@
         required: true,
         label: 'Firstname',
         align: 'left',
-        field: row => row.cells[2].displayValue,
+        field: 'firstname',
         format: val => `${val}`,
         sortable: true
       },
-      { name: 'Lastname', align: 'left', label: 'Lastname', field: row => row.cells[3].displayValue, sortable: true },
-      { name: 'eMail', align: 'left', label: 'eMail', field: row => row.cells[4].displayValue, sortable: true },
-      { name: 'Gender', align: 'center', label: 'Gender', field: row => row.cells[5].displayValue },
-      { name: 'Country', align: 'left', label: 'Country', field: row => row.cells[6].displayValue, sortable: true  },
-      { name: 'State', align: 'left', label: 'State', field: row => row.cells[7].displayValue, sortable: true  },
-      { name: 'City', align: 'left', label: 'City', field: row => row.cells[8].displayValue, sortable: true  },
-      { name: 'Street', align: 'left', label: 'Street', field: row => row.cells[9].displayValue },
-      { name: 'Zip', align: 'left', label: 'Zip', field: row => row.cells[10].displayValue },
-      { name: 'ARR', align: 'right', label: 'ARR', field: row => row.cells[1].displayValue, sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)}
+      { name: 'Lastname', align: 'left', label: 'Lastname', field: 'Lastname', sortable: true },
+      { name: 'eMail', align: 'left', label: 'eMail', field: 'eMail', sortable: true },
+      { name: 'Gender', align: 'center', label: 'Gender', field: 'Gender' },
+      { name: 'Country', align: 'left', label: 'Country', field: 'Country', sortable: true  },
+      { name: 'State', align: 'left', label: 'State', field: 'State', sortable: true  },
+      { name: 'City', align: 'left', label: 'City', field: 'City', sortable: true  },
+      { name: 'Street', align: 'left', label: 'Street', field: 'Street' },
+      { name: 'Zip', align: 'left', label: 'Zip', field: 'Zip' },
+      { name: 'ARR', align: 'right', label: 'ARR', field: 'ARR', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)}
     ]
 
     const loadData = () => {
         api.get('sheets/6040469330939780')
           .then((response) => {
             tablerows.value = response.data.rows
-            console.log(response.data.rows);
           })
           .catch(() => {
             $q.notify({
@@ -72,5 +98,39 @@
         
       } 
       loadData();
+
+      const groupData = computed(() => {
+          const grouped = {};
+
+          // Group data by country
+          tablerows.value.forEach(record => {
+            console.log('value here!');
+            console.log(mygroup.value);
+          if (!grouped[record.cells[mygroup.value].displayValue]) {
+           grouped[record.cells[mygroup.value].displayValue] = [];
+          }
+
+          grouped[record.cells[mygroup.value].displayValue].push({
+              'Country': record.cells[6].displayValue,
+              'Firstname': record.cells[2].displayValue,
+              'Lastname': record.cells[3].displayValue,
+              'eMail': record.cells[4].displayValue,
+              'Gender': record.cells[5].displayValue,
+              'Country': record.cells[6].displayValue,
+              'State': record.cells[7].displayValue,
+              'City': record.cells[8].displayValue,
+              'Street': record.cells[9].displayValue,
+              'Zip': record.cells[10].displayValue,
+              'ARR': record.cells[1].displayValue
+            });
+          });
+
+          console.log(grouped)
+          return grouped;
+      });
+
+      const getTotalArr = (records) => {
+        return records.reduce((total, record) => total + parseFloat(record.ARR), 0).toFixed(2);
+      };
 
 </script>
